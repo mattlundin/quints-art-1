8bea534516d0b9be095eceb774320a884f8b2880
Matt Lundin <matt.lundin@outlook.com>
2022-08-16 11:53:24 -0500

    gsap.js

--- src/gsap.js
+++ src/gsap.js
@@ -1,5 +1,5 @@
 import { gsap } from 'gsap';
-import { ScrollTrigger } from 'gsap/dist/ScrollTrigger.min.js';
+import { ScrollTrigger } from 'gsap/dist/ScrollTrigger.js';
 
 if (typeof window !== 'undefined') {
 	gsap.registerPlugin(ScrollTrigger);
